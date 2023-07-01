e500547bae2742932648e037a70a3ece24f2ef0a
Ethan Flatt <ethanflatt1@gmail.com>
2023-07-01 00:11:51 -0500

    Plan Controller

--- server/controllers/planController.js
+++ server/controllers/planController.js
@@ -1,12 +1,43 @@
 import { Plan } from  '../models'
 
 const getPlans = async (req, res) => {
-   try { const plans = await Plan.find()
-    return res.status(200).json({plans})
-} catch (error) {
-    return res.status(500).json({ message: error.message })
-    }
+    try { const plans = await Plan.find()
+        return res.status(200).json({plans})
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+        }
 }
 
+const getPlanById = async (req, res) => {
+    try { const plan = await Plan.findById(req.params.id)
+        return res.status(200).json({plan})
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+        }
+}
 
-export default { getPlans }
+const deletePlan = async (req, res) => {
+    try { const plan = await Plan.findByIdAndDelete(req.params.id)
+        return res.status(200).json({plan})
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+        }
+}
+
+ const createPlan = async (req, res) => {
+    try { const plan = await Plan.create(req.body)
+        return res.status(200).json({plan})
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+        }
+}
+
+const updatePlan = async (req, res) => {
+    try { const plan = await Plan.findByIdAndUpdate(req.params.id, req.body)
+        return res.status(200).json({plan})
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+        }
+}
+
+export default { getPlans, getPlanById, deletePlan, createPlan, updatePlan }
