363cefcf6fe3f844d4259d74ce80452ad0d078fb
chrischen <chrischen96@outlook.com>
2023-07-02 08:32:58 -0700

    add links and routes

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,6 +5,9 @@ const Nav = () => {
   return (
     <div className='nav'>
       <Link to='/'>Home</Link>
+      <Link to='/resorts'>Resorts</Link>
+      <Link to='/myfavorites'>My Favorites</Link>
+      <Link to='/login'>Login</Link>
     </div>
   )
 }
