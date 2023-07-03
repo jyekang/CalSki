97c4169f5dbba325d7b587117c88b1c4fe0f5aad
Ethan Flatt <ethanflatt1@gmail.com>
2023-07-03 09:01:21 -0500

    db imported

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import AppRouter from './routes/AppRouter.js';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import db from './db/index.js'
 import mongoose from 'mongoose';
 import router from './routes/AppRouter.js';
 
