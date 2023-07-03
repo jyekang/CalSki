aa1190364ee086dba3816b2e8911a43a59409973
chrischen <chrischen96@outlook.com>
2023-07-03 09:20:59 -0700

    navbar collapse

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './scss/custom.scss'
+import * as bootstrap from 'bootstrap'
 import './index.css'
-import {BrowserRouter} from'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
