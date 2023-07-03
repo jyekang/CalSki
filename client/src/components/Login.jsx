c1485cabbc72c787c13500669704f02003cc8557
jyekang <janicekangaroo@gmail.com>
2023-07-03 10:14:44 -0700

    login page template included

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
-
-const Login = () => {
+import React from 'react';
+ // from https://mdbootstrap.com/docs/react/extended/login-form/ 
+function App() {
   return (
-    <div>Login</div>
-  )
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+
+      <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
+      <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
+
+      <div className="d-flex justify-content-between mx-3 mb-4">
+        <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+        <a href="!#">Forgot password?</a>
+      </div>
+
+      <MDBBtn className="mb-4">Sign in</MDBBtn>
+
+      <div className="text-center">
+        <p>Not a member? <a href="#!">Register</a></p>
+        <p>or sign up with:</p>
+
+        <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
+          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon='facebook-f' size="sm"/>
+          </MDBBtn>
+
+          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon='twitter' size="sm"/>
+          </MDBBtn>
+
+          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon='google' size="sm"/>
+          </MDBBtn>
+
+          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon='github' size="sm"/>
+          </MDBBtn>
+
+        </div>
+      </div>
+
+    </MDBContainer>
+  );
 }
 
-export default Login
+export default App;
+
+
+// import React from 'react'
+
+
+
+// const Login = () => {
+//   return (
+//     <div>Login</div>
+//   )
+// }
+
+// export default Login
