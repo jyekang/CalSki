542621b4b5d462b2b19f51ca622a33df51fc641b
chrischen <chrischen96@outlook.com>
2023-07-04 03:06:20 -0700

    style resort list, navbar auto collapse

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,29 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
+import { useEffect } from 'react'
 
 const Nav = () => {
+  const handleClick = (e) => {
+    document.querySelector('.navbar-toggler').click()
+  }
+
   return (
     <div className='nav-bar'>
       <nav className="navbar navbar-expand-md bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#"><img src={logo} alt='logo' className='logo'/></a>
+          <a className="navbar-brand" href="/"><img src={logo} alt='logo' className='logo'/></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-md-0">
               <li className="nav-item">
-                <Link className='nav-link active' to='/'>Home</Link>
+                <Link className='nav-link active' to='/' onClick={handleClick}>Home</Link>
               </li>
               <li className="nav-item">
-                <Link className='nav-link' to='/resorts'>Resorts</Link>
+                <Link className='nav-link' to='/resorts' onClick={handleClick}>Resorts</Link>
               </li>
               <li className="nav-item">
-                <Link className='nav-link' to='/plans'>My Plans</Link>
+                <Link className='nav-link' to='/plans' onClick={handleClick}>My Plans</Link>
               </li>
               <li className="nav-item">
-              <Link className='nav-link' to='/login'>Login</Link>
+              <Link className='nav-link' to='/login' onClick={handleClick}>Login</Link>
               </li>
             </ul>
           </div>
--- client/src/components/Resorts.jsx
+++ client/src/components/Resorts.jsx
@@ -37,24 +37,26 @@ const Resorts = () => {
         <input type='text' placeholder='Search for a resort' className='search-bar' onChange={handleChange} value={search}/>
         <button type='submit' onClick={handleSearch}>Search</button>
       </form>
-      <div className='container'>
-        <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3'>
+      <div className='result-list'>
+        <div className='result-grid'>
           {
             resorts.map(resort => (
-              <div className='col' key={resort._id}>
-                <div className='card p-0'>
-                  <img src={`${resort.image}`} alt={resort.resortName} className='resort-img' onClick={() => navigate(`/resorts/${resort._id}`)}/>
-                  <div className='card-body text-start'>
-                    <h6 className='card-title fw-bold'>{resort.resortName}</h6>
-                    <p className='card-text'>{resort.region}</p>
+              <div className='result-item' key={resort._id}>
+                <div className='item-card'>
+                  <img src={`${resort.image}`} alt={resort.resortName} className='item-img' onClick={() => navigate(`/resorts/${resort._id}`)}/>
+                  <div className='item-content text-start'>
+                    <h6 className=''>{resort.resortName}</h6>
+
+                      <p className='p-0 m-0'>{resort.contactAddress}</p>
+                      <p className='p-0 mt-auto mb-0'>{resort.region}</p>
+                      
+                    </div>
                   </div>
                 </div>
-              </div>
             ))
           }
         </div>
       </div>
-
     </div>
   )
 }
