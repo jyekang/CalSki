756edbaf056b6c4d997d6bd3ff639d7682f484ee
Ethan Flatt <ethanflatt1@gmail.com>
2023-07-05 10:21:20 -0500

    starting plans component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
     <div className='home'>
         <div className='home-content-1 position-relative'>
             <h1 className='p-5 text-white'>CalSki</h1>
-            <h2 className='text-white'>Welcome to CalSki! Plan your dream vacation at one of California's 26 ski resorts. </h2>
+            <h2 className='text-black'>Welcome to CalSki! Plan your dream vacation at one of California's 26 ski resorts. Our website provides a once-in-a-lifetime opportunity to plan your perfect trip to one of these world-renowned ski resorts. We provide info associated with each location, so you know everything you can. All you have to do is make an account, and you can get started on making your ideal plan.</h2>
         </div>
         <div className='home-content-2 position-relative'>
-            <h1 className='p-5 text-white'>Just make an account, search for a resort, make a plan, and boom! You're done.</h1>
+            <h1 className='p-5 text-white'></h1>
             <h2 className=''></h2>
         </div>
     </div>
--- client/src/components/Plans.jsx
+++ client/src/components/Plans.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Plans = () => {
-  return (
-    <div>Plans</div>
-  )
+
+  const [plans, setPlan] = useState()
+
+  useEffect(() => {
+    const getPlan = async() => {
+      const response = await axios.get('http://localhost:3001/api/plans')
+      setPlan(response.data)
+      console.log(response)
+    }
+    getPlan()
+  },[])
+
+  return plans ? (
+    <div className="plansPage">
+      <h1>Plans</h1>
+      {
+        plans.map((plan, key) => (
+          <div className="container" key={plan._id}>
+            <div className="table-responsive">
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th>Resort</th>
+                    <th>Date</th>
+                    <th>Plan</th>
+                    <th>Update and delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>I want to go here</td>
+                    <td><button class="btn btn-success" id="updateButton">Update</button><button class="btn btn-danger" id="deleteButton">Delete</button></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  ) : (<h3>Looking for plans...</h3>)
 }
 
 export default Plans
