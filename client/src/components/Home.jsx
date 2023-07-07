10718753fd9acde84500d8007520398e321e6e36
Ethan Flatt <ethanflatt1@gmail.com>
2023-07-07 08:40:52 -0500

    Changed Home paragraphs

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,13 +6,13 @@ const Home = () => {
       <div className='home-content-1 d-flex'>
         <div className="text-wrapper d-flex flex-column align-items-center">
           <h1 className='text-white'>Welcome to CalSki!</h1>
-          <h3 className='text-white p-5 pt-0 lh-base'>We provide everything you should know to plan your perfect trip to one of the 26 world-renowned ski resorts in California.</h3>
+          <h3 className='text-white p-5 pt-0 lh-base'>We provide everything you need to know to plan your perfect trip to one of California's 26 world-renowned ski resorts.</h3>
           <h3 className="text-white fs-5"><Link to='/resorts' className='text-white icon-link link-offset-1 link-underline-light link-underline-opacity-0 link-underline-opacity-100-hover'>Find a Resort<i className="bi bi-chevron-right"></i></Link></h3>
         </div>
       </div>
       <div className='home-content-2 d-flex flex-column'>
         <h1 className='text-white fs-1'>Personalize your Experience</h1>
-        <h3 className='text-white ps-3 pe-3 lh-base'>All you have to do is to make an account. Just make an ideal plan and enjoy your advanturous vacation! </h3>
+        <h3 className='text-white ps-3 pe-3 lh-base'>All you have to do is make an account, make a plan, and enjoy your vacation! </h3>
         <h3 className="text-white mt-1 fs-5"><Link to='/login' className='text-white icon-link link-underline-light link-underline-opacity-0 link-underline-opacity-100-hover'><span>Login or Register</span><i className="bi bi-chevron-right"></i></Link></h3>
       </div>
     </div>
